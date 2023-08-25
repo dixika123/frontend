921b143d351a850de3e89ec8ca467bba424ee883
dixika123 <143152248+dixika123@users.noreply.github.com>
2023-08-25 10:04:11 +0545

    fix protected route

--- src/protected/UserRoute.jsx
+++ src/protected/UserRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const UserRoute = () => {
-    const user=localStorage.getItem('user')
+    const user=JSON.parse(localStorage.getItem('user'))
     return user!==null ? <Outlet/> : <Navigate to='/login'/>
 }
 
